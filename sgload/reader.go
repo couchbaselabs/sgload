3905bcb40c5502d8f33f4871133fcb19d25a2f44
Traun Leyden <tleyden@couchbase.com>
2016-09-20 12:21:52 -0700

    Factor out pullMoreDocs()

--- sgload/reader.go
+++ sgload/reader.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"strings"
 	"sync"
+	"time"
 
 	sgreplicate "github.com/couchbaselabs/sg-replicate"
 )
@@ -52,7 +53,9 @@ func (r *Reader) Run() {
 	}
 
 	numDocsPulled := 0
+	numDocsPulledJustNow := 0
 	since := StringSincer{}
+	var err error
 
 	for {
 
@@ -68,29 +71,46 @@ func (r *Reader) Run() {
 			return
 		}
 
-		changes, newSince, err := r.DataStore.Changes(since, r.BatchSize)
+		since, numDocsPulledJustNow, err = r.pullMoreDocs(since)
 		if err != nil {
 			panic(fmt.Sprintf("Got error getting changes: %v", err))
 		}
-		if newSince.Equals(since) {
-			panic(fmt.Sprintf("Since value should have changed from: %v", since))
-		}
-		since = newSince.(StringSincer)
-
-		// Strip out any changes with id "id":"_user/*"
-		// since they are user docs and we don't care about them
-		changes = stripUserDocChanges(changes)
-
-		bulkGetRequest := getBulkGetRequest(changes)
-
-		err = r.DataStore.BulkGetDocuments(bulkGetRequest)
-		if err != nil {
-			panic(fmt.Sprintf("Got error getting bulk docs: %v", err))
-		}
-
-		numDocsPulled += len(bulkGetRequest.Docs)
+		numDocsPulled += numDocsPulledJustNow
 
 	}
+
+}
+
+func (r *Reader) pullMoreDocs(since Sincer) (StringSincer, int, error) {
+
+	// numRetries := 5
+	// sleepMsBetweenRetry := 50
+	// retrySleeper := CreateDoublingSleeperFunc(numRetries, sleepMsBetweenRetry)
+
+	changes, newSince, err := r.DataStore.Changes(since, r.BatchSize)
+	if err != nil {
+		return StringSincer{}, 0, err
+	}
+
+	if len(changes.Results) == 0 {
+		logger.Warn("No changes pulled", "since", since)
+	}
+	if newSince.Equals(since) {
+		logger.Warn("Since value should have changed", "since", since, "newsince", newSince)
+	}
+
+	// Strip out any changes with id "id":"_user/*"
+	// since they are user docs and we don't care about them
+	changes = stripUserDocChanges(changes)
+
+	bulkGetRequest := getBulkGetRequest(changes)
+
+	err = r.DataStore.BulkGetDocuments(bulkGetRequest)
+	if err != nil {
+		return StringSincer{}, 0, err
+	}
+
+	return newSince.(StringSincer), len(bulkGetRequest.Docs), nil
 
 }
 
@@ -121,4 +141,63 @@ func stripUserDocChanges(changes sgreplicate.Changes) (changesStripped sgreplica
 	}
 
 	return changesStripped
+}
+
+// A retry sleeper is called back by the retry loop and passed
+// the current retryCount, and should return the amount of milliseconds
+// that the retry should sleep.
+type RetrySleeper func(retryCount int) (shouldContinue bool, timeTosleepMs int)
+
+// A RetryWorker encapsulates the work being done in a Retry Loop.  The shouldRetry
+// return value determines whether the worker will retry, regardless of the err value.
+// If the worker has exceeded it's retry attempts, then it will not be called again
+// even if it returns shouldRetry = true.
+type RetryWorker func() (shouldRetry bool, err error, value interface{})
+
+func RetryLoop(description string, worker RetryWorker, sleeper RetrySleeper) (error, interface{}) {
+
+	numAttempts := 1
+
+	for {
+		shouldRetry, err, value := worker()
+		if !shouldRetry {
+			if err != nil {
+				return err, nil
+			}
+			return nil, value
+		}
+		shouldContinue, sleepMs := sleeper(numAttempts)
+		if !shouldContinue {
+			if err == nil {
+				err = fmt.Errorf("RetryLoop for %v giving up after %v attempts", description, numAttempts)
+			}
+			logger.Warn("RetryLoop giving up", "description", description, "numAttempts", numAttempts)
+			return err, value
+		}
+		logger.Warn("RetryLoop will retry soon", "description", description, "sleepMs", sleepMs)
+
+		<-time.After(time.Millisecond * time.Duration(sleepMs))
+
+		numAttempts += 1
+
+	}
+}
+
+// Create a RetrySleeper that will double the retry time on every iteration and
+// use the given parameters
+func CreateDoublingSleeperFunc(maxNumAttempts, initialTimeToSleepMs int) RetrySleeper {
+
+	timeToSleepMs := initialTimeToSleepMs
+
+	sleeper := func(numAttempts int) (bool, int) {
+		if numAttempts > maxNumAttempts {
+			return false, -1
+		}
+		if numAttempts > 1 {
+			timeToSleepMs *= 2
+		}
+		return true, timeToSleepMs
+	}
+	return sleeper
+
 }
