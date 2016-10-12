286e0c5e776e9eaaa294b07ffeae832a2117195e
Traun Leyden <tleyden@couchbase.com>
2016-10-12 16:25:07 -0700

    Add expvar endpoint to all sgload commands

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "github.com/couchbaselabs/sgload/cmd"
+import (
+	"net/http"
+
+	_ "expvar"
+
+	"github.com/couchbaselabs/sgload/cmd"
+)
 
 func main() {
+
+	// Expose expvars via http -- needed by mobile-testkit to figure out
+	// when the process is finished.
+	go func() {
+		http.ListenAndServe(":9876", http.DefaultServeMux)
+	}()
+
 	cmd.Execute()
 }
