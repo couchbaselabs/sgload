09275d216cfdd64ff9273e90ceddfdb78719086c
Traun Leyden <tleyden@couchbase.com>
2016-11-23 17:48:55 -0800

    Log warning if cannot bind to port 9876 for expvars

--- main.go
+++ main.go
@@ -6,14 +6,20 @@ import (
 	_ "expvar"
 
 	"github.com/couchbaselabs/sgload/cmd"
+	"github.com/couchbaselabs/sgload/sgload"
 )
 
 func main() {
 
+	logger := sgload.Logger()
+
 	// Expose expvars via http -- needed by mobile-testkit to figure out
 	// when the process is finished.
 	go func() {
-		http.ListenAndServe(":9876", http.DefaultServeMux)
+		err := http.ListenAndServe(":9876", http.DefaultServeMux)
+		if err != nil {
+			logger.Warn("Unable to listen on port 9876.  Expvars won't be exposed", "err", err)
+		}
 	}()
 
 	cmd.Execute()
